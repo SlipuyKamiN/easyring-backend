06b99401c71f4a2a2a35a026503f3458ed7af9df
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-05-24 22:19:02 +0200

    fix: deleteUser

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -2,20 +2,27 @@ import { ctrlWrapper, HttpError } from "../../utils/index.js";
 import User from "../../models/users.js";
 
 const deleteUser = async (req, res) => {
-  const { id: _id } = req.params;
-  const { role } = req.user;
+  const { id } = req.params;
+  const { _id, role } = req.user;
 
-  const user = await User.findById(_id);
+  const user = await User.findById(id);
 
   if (!user) {
     throw HttpError(404, "User with such id was not found");
+  }
+
+  if (id === _id) {
+    throw HttpError(
+      404,
+      "You can't delete account of yourself. Contact admin."
+    );
   }
 
   if (role !== "admin") {
     throw HttpError(403, "You are not authorized to delete this user");
   }
 
-  const data = await User.findByIdAndDelete(_id);
+  const data = await User.findByIdAndDelete(id);
 
   res.json({
     success: true,
