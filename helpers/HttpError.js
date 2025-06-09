ad60fe5d8a8d451a5ee30b88e21b6db1caa1d9e0
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-06-09 15:04:56 +0200

    fix: sendTelegram on error

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,5 +1,3 @@
-import { sendTelegram } from "./index.js";
-
 const messageList = {
   400: "Bad Request",
   401: "Unauthorized",
@@ -12,7 +10,6 @@ const HttpError = (status, message = messageList[status]) => {
   const error = new Error(message);
   error.status = status;
 
-  sendTelegram(message, "error");
   return error;
 };
 
